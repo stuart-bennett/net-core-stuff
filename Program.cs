ce4674a87944678cebb719de9f22cdf099006caa
stuart-bennett <stuart.bennett@live.co.uk>
2017-06-04 22:27:52 +0100

    Remove hello world bit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+namespace net_core
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls("http://172.17.0.5:5000")
+                .Build();
+
+            host.Run();
         }
     }
 }
