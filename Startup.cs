7ed2bb1a0e779a648123374031bd2f77fa719d91
stuart-bennett <stuart.bennett@live.co.uk>
2017-06-05 22:09:43 +0100

    No Authentication on Hangfire dashboard
    This will eventually be replaced by requiring something more specific
    (perhaps a specific claim)

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hangfire;
+using Hangfire.Dashboard;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,13 +13,18 @@ using Nancy.Owin;
 
 namespace net_core
 {
+    public class AnonymousAuthFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context) => true;
+    }
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHangfire(x => x.UseSqlServerStorage("Server=172.17.0.6;User Id=sa;Password=aaaAAA!!;Database=TestDb;Trusted_Connection=False;"));
+            services.AddHangfire(x => x.UseSqlServerStorage("Server=db;User Id=sa;Password=aaaAAA!!;Database=TestDb;Trusted_Connection=False;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,9 +35,12 @@ namespace net_core
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
-            app.UseHangfireDashboard();
-            app.UseHangfireServer();
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new [] { new AnonymousAuthFilter() }
+            });
 
+            app.UseHangfireServer();
             app.UseOwin(x => {
                 x.UseNancy();
             });
